34d63cd2e7591bd1bcfb18f852963765c4f3320f
HUGO RAFAEL MONTEIRO <hugo.monteiro2>
2026-03-06 11:44:56 -0300

    Organização projeto

---
+++ src/app/ex-02/page.jsx
@@ -0,0 +1,10 @@
+function exemplo02 () {
+    return (
+        <div> 
+            <h1> Exemplo 1</h1>
+            <p> Componente basico para renderizar tela com react</p>
+        </div>
+    )
+
+}
+export default exemplo02 ;
--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,21 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-const Card = () => {
-  return (
-    <div>
-      <h3>Aula introdução</h3>
-      <h3>Aula Estilização</h3>
-      <h3>Aula Componentes</h3>
-    </div>
-  );
-}
-
 function Home() {
   return (
-    <div>
+    <div className={styles.containerExemplo}>
       <h1>React com Next JS</h1>
-      <h2> Aula de introdução</h2>
-      <h3> Leiaute e Estilização</h3>
-      <p> Nesta aula iremos entender o uso de CSS global e local.</p>
+      <h2>Aula de introdução</h2>
+      <h3>Leiaute e Estilização</h3>
+      <p>Nesta aula iremos entender o uso de CSS global e local.</p>
+      <label>Exemplo Label</label>
+      <small>Exemplo Small</small>
+      <div className={styles.conteinerExPosicao}>
+        <div className={styles.divVermelha}></div>
+        <div className={styles.divVerde}></div>
+      </div>
     </div>
   );
 }
-
 
 export default Home;
