efb149b3059f0e7b2ab8a1491d11f86a417d3974
HUGO RAFAEL MONTEIRO <hugo.monteiro2>
2026-03-13 13:42:01 -0300

    atividade 01 base

---
+++ src/app/atividades/atv-1/page.jsx
@@ -0,0 +1,10 @@
+function exemplo01 () {
+    return (
+        <div> 
+            <h1> Exemplo 1</h1>
+            <p> Componente basico para renderizar tela com react</p>
+        </div>
+    )
+
+}
+export default exemplo01 ;
